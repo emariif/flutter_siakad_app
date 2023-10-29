55d562c40e36c50af23cf819dbbd7730473b1001
emariif <okbro8788@gmail.com>
2023-10-29 13:18:39 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_siakad_app/bloc/login/login_bloc.dart';
 import 'package:flutter_siakad_app/pages/auth/splash_page.dart';
 
